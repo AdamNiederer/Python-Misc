17aa341aaf624a5ae8a4470b2c4b754f3b58e61e
Joshua <jrl5696@psu.edu>
2016-03-23 13:57:36 -0400

    Added bubblesort!

--- Sorting Algorithm Implementations/sorttools.py
+++ Sorting Algorithm Implementations/sorttools.py
@@ -8,5 +8,7 @@ def generate_randoms(size=10):
 def is_sorted(nums):
 	for i in range(len(nums) - 1):
 		if nums[i] > nums[i+1]:
+			raise Exception("Found unsorted pair [{}, {}] that spans indices ({} - {})"
+				.format(nums[i], nums[i+1], i, i+1))
 			return False
 	return True
